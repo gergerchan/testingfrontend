6b45826541e40a88acbe4be66c69c3f217704dd6
Gerry Andika Chandra <gergerchandra@gmail.com>
2021-03-03 14:22:05 +0800

    [Gerry] Membuat page register dan goal

--- server.js
+++ server.js
@@ -1,4 +1,12 @@
 const express = require("express")
 const app = express()
 
+app.use(express.static("assets"))
+app.set("view engine", "ejs")
+app.get("/register", (req,res) => {
+    res.render("register", {judul : "Register Akun", css:"css/register.css"})    
+})
+app.get("/goal", (req,res) => {
+    res.render("goal", {judul : "Goal kamu", css:"css/goal.css"})    
+})
 app.listen(3000, () => console.log("Server jalan di Port 3000") )
